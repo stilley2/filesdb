db12ee6c70d00d9f6997310edb8de55058728ea8
Steven Tilley <stilley2@jhmi.edu>
2017-08-18 16:32:20 -0400

    setup.py cleanup and pytest support

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup
 
 setup(name='filesdb',
       version='1.0.0',
-      description='A simple tool for tracking files in a database',
+      description='A simple tool for tracking files using a database',
       author='Steven Tilley',
       author_email='steventilleyii@gmail.com',
       packages=['filesdb'],
       entry_points={'console_scripts': ['filesdb = filesdb.__init__:main']},
+      python_requires='>=3',
+      tests_require=['pytest'],
+      setup_requires=['pytest-runner'],
       )
