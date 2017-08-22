387de238d16203d366973142329ea0901fe1e1cb
Steven Tilley <stilley2@jhmi.edu>
2017-08-22 11:22:50 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='filesdb',
-      version='0.2.0',
+      version='0.2.3',
       description='A simple tool for tracking files',
       author='Steven Tilley',
       author_email='steventilleyii@gmail.com',
