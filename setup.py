8c8e54a05a902031b875325acc7e569b9461a42f
Steven Tilley <stilley2@jhmi.edu>
2018-06-04 21:17:02 -0400

    add test_filesdb.py to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 
 setup(name='filesdb',
-      version='2.1.1',
+      version='2.1.2',
       description='A simple tool for tracking files',
       author='Steven Tilley',
       author_email='steventilleyii@gmail.com',
+      scripts=['test_filesdb.py'],
       packages=['filesdb'],
       entry_points={'console_scripts': ['filesdb = filesdb.__main__:main']},
       python_requires='>=3',
