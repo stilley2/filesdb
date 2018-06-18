a4bf0b24d5054ed3d05dd795b8cb455146e377c4
Steven Tilley <stilley2@jhmi.edu>
2018-06-18 08:08:31 -0400

    add test dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='filesdb',
-      version='2.1.4',
+      version='2.1.5',
       description='A simple tool for tracking files',
       author='Steven Tilley',
       author_email='steventilleyii@gmail.com',
